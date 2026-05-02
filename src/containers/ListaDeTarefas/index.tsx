553a5541f351fd1dcf552aae795309a9157a5879
jonathan <jonathan2500@outlook.pt>
2026-05-02 18:18:13 -0300

    feat: update project dependencies and add new components
    - Added @reduxjs/toolkit and react-redux for state management.
    - Introduced styled-components for styling and global styles.
    - Created BarraLateral and ListaDeTarefas containers for layout.
    - Implemented FiltroCard component for task filtering.
    - Removed unused App.css and index.css files.
    - Updated index.html to include Google Fonts.
    - Refactored App component to use new structure and components.
    - Removed old test files and logo.svg.
    - Updated package-lock.json and package.json for dependency versions.

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,9 @@
+import exp from 'constants'
+
+const ListaDeTarefas = () => (
+  <main>
+    <p>2 tarefas maracadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
+  </main>
+)
+
+export default ListaDeTarefas
